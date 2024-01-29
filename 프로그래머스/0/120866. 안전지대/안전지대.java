62dbdd0d5b7e6d62a96106d0a008344ca24b3c4e
DEA WON KIM <108449634+kdw4831@users.noreply.github.com>
2024-01-29 21:45:09 +0900

    [level 0] Title: 안전지대, Time: 0.16 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board.length; j++){
                 
-                if(board[i][j]==1 && board.length !=1){
+                if(board[i][j]==1 && board.length !=1){//??
                     if(i==0 && j==0){
                         board[i][j+1]=(board[i][j+1]==1)?1:danger;
                         board[i+1][j]=(board[i+1][j]==1)?1:danger;
