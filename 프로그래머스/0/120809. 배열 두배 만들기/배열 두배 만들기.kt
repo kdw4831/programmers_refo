d5c898f433e0c79ef649ef25a8889c0c7d71f34a
DEA WON KIM <108449634+kdw4831@users.noreply.github.com>
2025-03-27 10:44:47 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.04 ms, Memory: 63.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(numbers: IntArray): IntArray {
+        for(i in 0 until numbers.size){
+            numbers[i] *= 2 
+        }
+        
+        return numbers
+    }
+}
