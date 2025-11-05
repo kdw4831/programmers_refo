e20dbc5ead62cd997a736a05e269acb760ad7aec
DEA WON KIM <108449634+kdw4831@users.noreply.github.com>
2025-11-05 16:31:52 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT f.flavor as flavor
+FROM first_half as f
+JOIN 
+    (SELECT flavor, sum(total_order) as total_order
+    FROM july 
+    GROUP BY flavor) as j
+ON f.flavor = j.flavor
+ORDER BY f.total_order+ j.total_order desc
+limit 3;
